8fd0d2471be85e209f903279159353239fb3f2b7
pedroAndrad1 <41485933+pedroAndrad1@users.noreply.github.com>
2019-03-24 20:48:57 -0300

    Add files via upload

---
+++ models/Historia.js
@@ -0,0 +1,19 @@
+class Historia {
+  constructor() {
+      this._comeco;
+      this._meio;
+      this._fim;
+  }
+
+  set comeco(comeco){
+      this._comeco = comeco;
+  }
+
+  set meio(meio){
+      this._meio = meio;
+  }
+
+  set fim(fim){
+    this._fim = fim;
+  }
+}
